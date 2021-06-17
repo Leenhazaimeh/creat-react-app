278591b44c14516c39fd63771592eedbd24e64e5
Leen Hazaimeh <leenhazaimeh94@gmail.com>
2021-06-17 23:05:08 +0300

    done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,10 @@
-
 import React from 'react';
 import Header from './components/header';
 import Main from './components/main';
 import Footer from './components/footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Data from './components/Data.json';
-import SelectedBeast from './components/SelectedBeast';
+import SelectedBeast from './components/SelectedBeast'
 
 
 class App extends React.Component {
@@ -17,17 +16,6 @@ class App extends React.Component {
       ,selectedBeast: {}
     }
   }
-
-
-  updateForm=(newHorn)=>{
-    this.setState({
-      Data:newHorn
-    })
-  }
-
-
-
-
   selectedUpdate=(title)=> {
     let selected=[];
     Data.forEach((beast)=>{
@@ -47,13 +35,8 @@ class App extends React.Component {
     return(
       <div>
         <Header />
-        {/* <UserForm
-        listHandle={this.listHandle}
-        /> */}
         <div  style={{display:'flex', 'flex-wrap': 'wrap' , gap:'0.8em' , 'justify-content':'center'}}>
         <Main
-        updateForm={this.updateForm}
-        
         Data={this.state.Data}
         selectedUpdate={this.selectedUpdate}
         />
@@ -72,3 +55,4 @@ class App extends React.Component {
 }
 
 export default App;
+
